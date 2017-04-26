1a5ea5a3755a78fba4b83e59aac1a70ed751b2ef
Rahul Gonsalves <rahul@uncommon.is>
2017-04-26 15:08:23 +0530

    Changed to be more useful for me.

--- src/os/install/macos/main.sh
+++ src/os/install/macos/main.sh
@@ -9,19 +9,11 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 ./xcode.sh
 ./homebrew.sh
 ./bash.sh
-./../nvm.sh
 
 ./browsers.sh
-./compression_tools.sh
 ./git.sh
-./gpg.sh
 ./image_tools.sh
 ./misc.sh
-./misc_tools.sh
-./../npm.sh
-./tmux.sh
 ./video_tools.sh
-./../vim.sh
-./web_font_tools.sh
 
 ./cleanup.sh
