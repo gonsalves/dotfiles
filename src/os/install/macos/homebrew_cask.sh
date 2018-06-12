fbfb8e3ef4a50cd9b0c9c7798ffb37f321f120a6
Rahul Gonsalves <rahul@uncommon.is>
2018-06-12 09:21:49 +0530

    Edited default apps
    This commit reflects my mid-2018 default app usage better.

--- src/os/install/macos/homebrew_cask.sh
+++ src/os/install/macos/homebrew_cask.sh
@@ -13,8 +13,6 @@ brew tap caskroom/cask
 brew_install 'Git' 'git'
 brew_install 'Mac App Store' 'mas'
 brew_install "Dropbox" "dropbox" "caskroom/cask" "cask"
-brew_install "Calibre" "calibre" "caskroom/cask" "cask"
-brew_install "Notational Velocity (Alt)" "nvalt" "caskroom/cask" "cask"
 brew_install "Sublime Text" "sublime-text" "caskroom/cask" "cask"
 brew_install "Alfred" "alfred" "caskroom/cask" "cask"
 
@@ -26,7 +24,6 @@ print_in_purple "\n   Internet\n"
 
 brew_install "Mozilla Firefox" "firefox" "caskroom/cask" "cask"
 brew_install "Google Chrome" "google-chrome" "caskroom/cask" "cask"
-brew_install "Google Hangouts" "google-hangouts" "caskroom/cask" "cask"
 
 printf "\n"
 
@@ -38,6 +35,7 @@ brew_install "Skype" "skype" "caskroom/cask" "cask"
 brew_install "Slack" "slack" "caskroom/cask" "cask"
 brew_install "Telegram" "telegram" "caskroom/cask" "cask"
 brew_install "WhatsApp Desktop" "whatsapp" "caskroom/cask" "cask"
+brew_install "Google Hangouts" "google-hangouts" "caskroom/cask" "cask"
 
 printf "\n"
 
@@ -48,10 +46,11 @@ print_in_purple "\n   Utilities\n"
 brew_install "TripMode" "tripmode" "caskroom/cask" "cask"
 brew_install "Moom" "moom" "caskroom/cask" "cask"
 brew_install "Flux" "flux" "caskroom/cask" "cask"
-brew_install "Github Desktop" "github-desktop" "caskroom/cask" "cask"
-brew_install "Hazel" "hazel" "caskroom/cask" "cask"
 brew_install "The Unarchiver" "the-unarchiver" "caskroom/cask" "cask"
 brew_install "iStat Menus" "istat-menus" "caskroom/cask" "cask"
+brew_install "VideoLAN Controller" "vlc" "caskroom/cask" "cask"
+brew_install "Unifi Controller" "ubiquiti-unifi-controller" "caskroom/cask-drivers" "cask"
+brew_install "Github Desktop" "github" "caskroom/cask" "cask"
 brew_install "Matt's TraceRoute (mtr)" "mtr"
     # allow mtr to run without sudo
     mtrlocation=$(brew info mtr | grep Cellar | sed -e 's/ (.*//')
@@ -69,6 +68,7 @@ print_in_purple "\n   Design Tools\n\n"
 brew_install "Sketch" "sketch" "caskroom/cask" "cask"
 brew_install "Zeplin" "zeplin" "caskroom/cask" "cask"
 brew_install "Icon Jar" "iconjar" "caskroom/cask" "cask"
+brew_install "Monotype Sky Fonts" "skyfonts" "caskroom/cask" "cask"
 brew_install 'FFmpeg' 'ffmpeg'
 brew_install 'ImageMagick' 'imagemagick --with-webp'
 
@@ -80,9 +80,10 @@ printf "\n"
 
 print_in_purple "\n   Business Software\n\n"
 
-brew_install "Quickbooks Desktop" "quickbooks" "caskroom/cask" "cask"
-brew_install "MacDown (Markdown Editor)" "macdown" "caskroom/cask" "cask"
+brew_install "Quickbooks Online" "quickbooks-online" "caskroom/cask" "cask"
 brew_install "Microsoft Office" "microsoft-office" "caskroom/cask" "cask"
+brew_install "Typora (Markdown Editor" "typora" "caskroom/cask" "cask"
+brew_install "Basecamp" "basecamp" "caskroom/cask" "cask"
 
 
 printf "\n"
